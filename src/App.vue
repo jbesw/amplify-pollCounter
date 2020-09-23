855a03626866bbe3008ec4b031950d2f9c6c2c16
EC2 Default User <ec2-user@ip-172-31-12-108.ec2.internal>
2020-09-23 20:58:50 +0000

    Adding code

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,12 @@
       <b-card-group deck>
         <b-card bg-variant="success" text-variant="white" header="Vote Leslie Knope" class="text-center" footer-tag="footer">
           <b-card-text>Leslie Knope</b-card-text>
-          <b-button size="lg" variant="primary"  @click="vote('Leslie')">Button</b-button>
+          <b-button size="lg" variant="primary"  @click="vote('Leslie', '{{ email }}', '{{ tel }}')">Button</b-button>
         </b-card>
 
         <b-card bg-variant="danger" text-variant="white" header="Vote Bobby Newport" class="text-center" footer-tag="footer">
           <b-card-text>Bobby Newport</b-card-text>
-          <b-button size="lg" variant="primary" @click="vote('Bobby')">Button</b-button>
+          <b-button size="lg" variant="primary" @click="vote('Bobby', '{{ email }}', '{{ tel }}')">Button</b-button>
         </b-card>
       </b-card-group>
     </b-row>
@@ -39,12 +39,12 @@ export default {
     }
   },
   methods: {
-    vote: async function (vote) {
+    vote: async function (vote, email, tel) {
       const init = {
         queryStringParameters: {
           vote,
-          this.email,
-          this.tel
+          email,
+          tel
         }
       }
       const response = await API.post(this.apiName, '/votes', init)
